aa6626a8ae381de45b3b6a1f1a8e137370632947
Taro Kawada <kawadataro@gmail.com>
2017-11-14 00:09:41 +0800

    App and CalculatorBody component

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CalculatorBody from './CalculatorBody'
+
+class App extends React.Component {
+    render(){
+        return(
+            <CalculatorBody>
+            </CalculatorBody>
+        )
+    }
+}
+
+export default App
