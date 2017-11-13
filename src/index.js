9c81b1d0a7f399b539636e91936d0db3aad897a2
Taro Kawada <kawadataro@gmail.com>
2017-11-14 00:09:20 +0800

    removed excessive boilerplates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import { Provider } from 'react-redux'
+// import { createStore, applyMiddleware } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App.jsx'
+
+//const store = createStore(() => [], {}, applyMiddleware())
+
+ReactDOM.render(
+    <App />, 
+    document.querySelector('#root')
+)
